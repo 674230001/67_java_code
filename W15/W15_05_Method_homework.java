2576670bbea07b3509757bbbbfbf99c9be82b7f5
674230001 <rapipat0611@gmail.com>
2024-10-27 22:08:19 +0700

    Add files via upload

---
+++ W15/W15_05_Method_homework.java
@@ -0,0 +1,30 @@
+
+package W15;
+
+import java.util.Scanner;
+
+public class W15_05_Method_homework {
+    static int calculate(int factorial) {
+        
+        int num = 1;
+        System.out.print("Factorial for " + factorial + " is ");
+        for (int i = 1; i <= factorial; i++) {
+            if (i < factorial) {
+                System.out.print(" x ");
+            } else {
+                System.out.print(" = ");
+            }
+            num = num * i;
+        }
+        factorial = num;
+        return factorial;
+    }
+
+    public static void main(String[] args) {
+        Scanner mj = new Scanner(System.in);
+        System.out.print("Enter Numbar ");
+        int factorial = mj.nextInt();
+        
+        System.out.println(calculate(factorial));
+    }
+}
